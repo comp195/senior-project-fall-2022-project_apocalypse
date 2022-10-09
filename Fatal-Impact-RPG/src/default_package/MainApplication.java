e3f12f66a64cf3c512cd7d46549f5ae0800b28c5
kevinnguyen <k_nguyen159@u.pacific.edu>
2022-10-08 18:31:05 -0700

    Added health points but it is not shown yet.

--- Fatal-Impact-RPG/src/default_package/MainApplication.java
+++ Fatal-Impact-RPG/src/default_package/MainApplication.java
@@ -1,5 +1,27 @@
 package default_package;
 
+import java.awt.Color;
+import java.util.ArrayList;
+
+import default_package.GButton;
+import default_package.GParagraph;
+import acm.graphics.GImage;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import javax.swing.Timer;
+import acm.graphics.GImage;
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
 public class MainApplication extends GraphicsApplication {
 	public static final int WINDOW_WIDTH = 800;
@@ -17,6 +39,17 @@ public class MainApplication extends GraphicsApplication {
 	private boolean playerWin;
 	private int count;
 	
+	
+	public Player player = new Player(0, 0, this);
+	//private ArrayList<Item> items = new ArrayList <Item>();
+	
+	// Player health GUI
+	private GParagraph healthPoints; 
+	private static final int heartRootX = 75, heartRootY = 610, heartWidth = 30;
+	ArrayList <GImage> playerHearts = new ArrayList <GImage>();
+	private Font customFont;
+	
+	
 
 	public void init() {
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -33,6 +66,65 @@ public class MainApplication extends GraphicsApplication {
 		switchToMenu();
 		 
 	}
+	
+	
+	/* GUI
+	 * -----------------------------
+	 * setGUI()
+	 * addGUI()
+	 * removeGUI()
+	 * updatePlayerHeartsGUI(int hp)
+	 */
+	
+	public void setGUI() {
+		healthPoints = new GParagraph("HP:", 45, 625);
+		healthPoints.setColor(Color.white); 
+		healthPoints.setFont(this.customFont);
+	}
+	public void addGUI() {
+		this.add(healthPoints);
+		updatePlayerHeartsGUI(this.player.getHP());
+	}
+	public void removeGUI() {
+		this.remove(healthPoints);
+		updatePlayerHeartsGUI(0);
+	}
+	
+	
+	public void updatePlayerHeartsGUI(int hp) {
+		int heartLen = playerHearts.size();
+		int dif = hp - heartLen;
+		if (dif > 0) {
+			for (int i = 0; i < dif; i++) {
+				GImage heart = new GImage("media/HP.png", heartRootX + ((heartLen + i) * heartWidth), heartRootY);
+				heart.setSize(25, 20);
+				heart.setVisible(true); 
+				playerHearts.add(heart);
+				this.add(heart); 
+			}
+		}
+		else if (dif < 0) {
+			dif = dif * -1; // Absolute value
+			for (int i = 0; i < dif; i++) {
+				int end = playerHearts.size() - 1;
+				GImage heart = playerHearts.get(end);
+				this.remove(heart);
+				playerHearts.remove(end);
+			}
+		}
+	}
+	
+	
+	/* public void grab(Item item)   {
+		this.player.grabItem(item);
+		item.setPickedUp(true);
+		this.remove(item.getImage());
+		this.add(item.getInvSprite());
+	} */
+	
+	
+	
+	
 
 	public void switchToMenu() {
 		count++;
--- Fatal-Impact-RPG/src/default_package/ScreenDisplayPane.java
+++ Fatal-Impact-RPG/src/default_package/ScreenDisplayPane.java
@@ -1,11 +1,13 @@
 package default_package;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.Timer;
+
 
 import acm.graphics.GImage;
 import acm.graphics.GLabel;
@@ -15,7 +17,6 @@ import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 //import edu.pacific.comp55.starter.MainApplication;
 
-// Second Commit by Eesa Khan
 
 public class ScreenDisplayPane extends GraphicsPane implements ActionListener {
 	
@@ -27,6 +28,14 @@ public class ScreenDisplayPane extends GraphicsPane implements ActionListener {
 	private int currentRoom; // to display current room
 	private static final int BACKGROUND_TILE_SIZE = 50;
 	private GImage map; 
+	
+
+	
+	private GParagraph healthPoints; 
+	private static final int heartRootX = 75, heartRootY = 610, heartWidth = 30;
+	ArrayList <GImage> playerHearts = new ArrayList <GImage>();
+	private Font customFont;
+	
 	
 	public ScreenDisplayPane(MainApplication app) {
 		super();
@@ -61,6 +70,50 @@ public class ScreenDisplayPane extends GraphicsPane implements ActionListener {
 	public static void main(String[] args) {
 		//new ScreenDisplayPane().start();
 	}
+	
+	
+	public void setGUI() {
+		healthPoints = new GParagraph("HP:", 45, 625);
+		healthPoints.setColor(Color.white); 
+		healthPoints.setFont(this.customFont);
+	}
+	public void addGUI() {
+		//this.add(healthPoints);
+		//updatePlayerHeartsGUI(this.player.getHP());
+	}
+	public void removeGUI() {
+		//this.remove(healthPoints);
+		updatePlayerHeartsGUI(0);
+	}
+	
+	
+	public void updatePlayerHeartsGUI(int hp) {
+		int heartLen = playerHearts.size();
+		int dif = hp - heartLen;
+		if (dif > 0) {
+			for (int i = 0; i < dif; i++) {
+				GImage heart = new GImage("media/HP.png", heartRootX + ((heartLen + i) * heartWidth), heartRootY);
+				heart.setSize(25, 20);
+				heart.setVisible(true); 
+				playerHearts.add(heart);
+				//this.add(heart); 
+			}
+		}
+		else if (dif < 0) {
+			dif = dif * -1; // Absolute value
+			for (int i = 0; i < dif; i++) {
+				int end = playerHearts.size() - 1;
+				GImage heart = playerHearts.get(end);
+				//this.remove(heart);
+				playerHearts.remove(end);
+			}
+		}
+	}
+	
+	
+	
+
+	
 
 	@Override
 	public void showContents() {
@@ -68,7 +121,9 @@ public class ScreenDisplayPane extends GraphicsPane implements ActionListener {
 		program.add(new GImage("Fi-short-ranged.PNG",BACKGROUND_TILE_SIZE, BACKGROUND_TILE_SIZE));
 		//setBackground("city-map.jpg");
 		// TODO Auto-generated method stub
-		
+		//program.player.getInventory();
+		//program.add(Inventory.INVENTORY_IMG, Inventory.INVENTORY_X, Inventory.INVENTORY_Y);
+		program.add(healthPoints);
 		
 		
 	}
@@ -77,6 +132,9 @@ public class ScreenDisplayPane extends GraphicsPane implements ActionListener {
 	public void hideContents() {
 		// TODO Auto-generated method stub
 		program.remove(map);
+		//program.remove(Inventory.INVENTORY_IMG);
+		program.removeGUI();
+		program.remove(healthPoints);
 		
 	}
 
