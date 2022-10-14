bca0ba255263ef3a54ef294a19ca180d1b626c53
Melkman68 <e_khan2@u.pacific.edu>
2022-10-13 17:05:04 -0700

    Edited Item class with basic methods (similar to Entity).

--- Fatal-Impact-RPG/src/default_package/Item.java
+++ Fatal-Impact-RPG/src/default_package/Item.java
@@ -1,73 +1,52 @@
 package default_package;
-//import Item.ItemType;
-
-/* import Item.MapType;
-import Item.RoomType;
 import acm.graphics.GImage;
-import acm.graphics.GRect;
+import acm.graphics.GLabel;
 
 public class Item {
+	private GImage sprite;
+	private String itemType;
+	private GLabel message;
+	//private static final String folder = "/images/"; // Later for creating folder for all images
 	
-	private int x, y, count, width = 32, height = 32;
-	private String name, desc;
-	private boolean used, pickedUp;
-	private GImage sprite, invSprite;
-	private ItemType type;
-	private RoomType roomType;
-	private MapType mapType;
-
-	public Item (String name, GImage sprite, ItemType type, MapType map) {
-		this.name = name;
-		this.sprite = sprite;
-		setInvSprite(new GImage("res/inventory/" + name + ".png", x, y));
-		this.type = type;
-		count = 1;  
-		this.mapType = map;
+	public Item(GImage image, String name) {
+		sprite = image;
+		itemType = name;
+		message = new GLabel("");
 	}
 	
-	public void setRoomType(RoomType roomType) {this.roomType=roomType;}
+	public void setSprite(GImage newImage) {
+		sprite = newImage;
+	}
 	
-	public RoomType getRoomType() {return roomType;}
-		
-	public MapType getMapType() {return mapType;}
+	public GImage getSprite() {
+		return sprite;
+	}
 	
-	public ItemType getItemType() {return type;}
+	public void setLabel(String s) {
+		message.setLabel(s);
+	}
 	
-	public int getX()   {return x;}
+	public GLabel getLabel() {
+		return message;
+	}
 	
-	public int getY()   {return y;}
+	public void setItemType(String s) {
+		itemType = s;
+	}
 	
-	public int getCount()   {return count;}
+	public String getItemType() {
+		return itemType;
+	}
 	
-	public String getName()   {return name;}
-	
-	public GImage getImage()   {return sprite;}
-	
-	public void setImage(GImage image)   {sprite = image;}
-	
-	public String getDescription()   {return desc;}
-	
-	public GRect getBounds()   {return new GRect(x, y, width, height);}
-	
-	public boolean isUsed()   {return used;}
-	
-	public boolean isPickedUp()   {return pickedUp;}
-	
-	public void setX(int x)   {this.x = x;}
-	
-	public void setY(int y)   {this.y = y;}
-	
-	public void setCount(int count)   {this.count = count;}
-	
-	public void setName(String name)   {this.name = name;}
-	
-	public void setDescription(String desc)   {this.desc = desc;}
-	
-	public void setUsed(boolean used)   {this.used = used;}
-	
-	public void setPickedUp(boolean pickedUp)   {this.pickedUp = pickedUp;}
+	public static void main(String[] args) {
 
-	public GImage getInvSprite() {return invSprite;}
+	}
 
-	public void setInvSprite(GImage invSprite) {this.invSprite = invSprite;}
-} */ 
+	//Method for returning folder with all images. Future concept.
+	/*
+	public static String getFolder() {
+		return folder;
+	}
+	*/
+
+}
